a548335812d94387e12b545aef3733e0414fe016
Balta <baltax.75@gmail.com>
2023-12-12 20:13:13 +0100

    Adds alert dialog and improves design

--- build-logic/plugins/src/main/java/Config.kt
+++ build-logic/plugins/src/main/java/Config.kt
@@ -1,7 +1,7 @@
 object Config {
     const val baseApplicationId = "com.baltajmn.flowtime"
-    const val versionCode = 6
-    const val versionName = "1.0.6"
+    const val versionCode = 7
+    const val versionName = "1.0.7"
 
     object Feature {
         const val Screens = "$baseApplicationId.features.screens"
