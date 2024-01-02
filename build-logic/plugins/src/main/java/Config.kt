c1d14165ff130353de2a0179609b048fa2af905e
Balta <baltax.75@gmail.com>
2024-01-02 21:12:51 +0100

    Changes version

--- build-logic/plugins/src/main/java/Config.kt
+++ build-logic/plugins/src/main/java/Config.kt
@@ -1,7 +1,7 @@
 object Config {
     const val baseApplicationId = "com.baltajmn.flowtime"
-    const val versionCode = 13
-    const val versionName = "1.0.13"
+    const val versionCode = 14
+    const val versionName = "1.0.14"
 
     object Feature {
         const val Screens = "$baseApplicationId.features.screens"
